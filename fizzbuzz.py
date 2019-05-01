20a712a9b0fc0f09bc0d48305037fa0ee55a8630
yadlra <yadi.sb1@gmail.com>
2019-05-01 11:27:26 +0100

    fizzbuzz-python

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizzbuzz(number):
+ if number % 5 == 0 and number % 3 == 0:
+     print ('fizzbuzz')
+ elif number % 5 == 0:
+     print ('buzz')
+ elif number % 3 == 0:
+     print ('fizz')
+
+fizzbuzz(1)
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
