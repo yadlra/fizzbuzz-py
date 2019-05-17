4790e4c3bce9e33ed868e7c0750832540b69b983
yadlra <yadi.sb1@gmail.com>
2019-05-17 14:46:58 +0100

    fizzbuzz with tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,8 @@
-def fizzbuzz(number):
- if number % 5 == 0 and number % 3 == 0:
-     print ('fizzbuzz')
- elif number % 5 == 0:
-     print ('buzz')
- elif number % 3 == 0:
-     print ('fizz')
+def divisibleby3(number):
+    return number % 3 == 0
 
-fizzbuzz(1)
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
+def divisibleby5(number):
+    return number % 5 == 0
+
+def __init__(self):
+     pass
---
+++ test.py
@@ -0,0 +1,13 @@
+import fizzbuzz
+import unittest
+
+
+def test_div_by_3():
+    assert fizzbuzz.divisibleby3(3) == True
+
+def test_div_by_5():
+    assert fizzbuzz,divisibleby5(5) == True
+
+
+if __name__ == "__main__":
+    unittest.main()
